43ab09a0e0f135193d2c5098635434966156e0ea
thearavind <thearuvi@gmail.com>
2019-05-07 01:29:21 +0530

    Enable get access token api call

--- src/Redirect.tsx
+++ src/Redirect.tsx
@@ -5,23 +5,23 @@ const LoginSuccess = (props: any) => {
   let defaultValue: boolean | undefined
   const [isAuthenticated, setIsAuthenticated] = useState(defaultValue);
   useEffect(() => {
-    // let params = new URLSearchParams(props.location.search)
-    // const authToken = params.get('code')
-    // fetch(`/generate_token`, {
-    //   method: 'POST',
-    //   body: JSON.stringify({ code: authToken })
-    // })
-    //   .then(res => res.json())
-    //   .then((response) => {
-    //     setIsAuthenticated(true)
-    //     document.cookie = `accessToken=${response.access_token};`
-    //     document.cookie = `refreshToken=${response.refresh_token};`
-    //     document.cookie = `expires=${new Date(new Date().getTime() + (10000 * 60 * 60 * 7 * 24))};`
-    //     document.cookie = `doamin=localhost;`
-    //   })
-    //   .catch(() => {
-    //     setIsAuthenticated(false)
-    //   })
+    let params = new URLSearchParams(props.location.search)
+    const authToken = params.get('code')
+    fetch(`/api/generate_token`, {
+      method: 'POST',
+      body: JSON.stringify({ code: authToken })
+    })
+      .then(res => res.json())
+      .then((response) => {
+        setIsAuthenticated(true)
+        document.cookie = `accessToken=${response.access_token};`
+        document.cookie = `refreshToken=${response.refresh_token};`
+        document.cookie = `expires=${new Date(new Date().getTime() + (10000 * 60 * 60 * 7 * 24))};`
+        document.cookie = `doamin=localhost;`
+      })
+      .catch(() => {
+        setIsAuthenticated(false)
+      })
   }, []);
 
   if (isAuthenticated === undefined) {
